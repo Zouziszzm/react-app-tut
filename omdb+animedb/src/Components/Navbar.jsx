e2b1474d94b8da25429fe7025e50d24ec132ae29
Al Farhaan khan I Inamdar <63239385+Zouziszzm@users.noreply.github.com>
2023-02-12 14:44:56 +0530

    omdb+Anime
    viewer.

---
+++ omdb+animedb/src/Components/Navbar.jsx
@@ -0,0 +1,11 @@
+function Navbar() {
+  return (
+    <div className="Nav-head">
+        <div className="">
+            <a className="nav-items" href="/">Movies</a>
+            <a className="nav-items" href="/">Anime</a>
+        </div>
+    </div>
+  )
+}
+export default Navbar
