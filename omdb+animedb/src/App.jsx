6a3d708367a0abe637367dc80c5868885b96aec9
Al Farhaan khan I Inamdar <63239385+Zouziszzm@users.noreply.github.com>
2023-02-13 14:48:29 +0530

    still in prog 03

--- omdb+animedb/src/App.jsx
+++ omdb+animedb/src/App.jsx
@@ -1,18 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [animeList, SetAnimeList] = useState([]);
+	const [topAnime, SetTopAnime] = useState([]);
+	const [search, SetSearch] = useState("");
 
-  return (
-    <div className="App">
-      <div className='head'>
-        <a className='m-items'>Anime</a>
-        <a className='m-items'>Movies</a>
-      </div>
-    </div>
-  )
+	const GetTopAnime = async () => {
+		const temp = await fetch(`https://api.jikan.moe/v3/top/anime/1/bypopularity`)
+			.then(res => res.json());
+
+		SetTopAnime(temp.top.slice(0, 5));
+	}
+
+	const HandleSearch = e => {
+		e.preventDefault();
+
+		FetchAnime(search);
+	}
+
+	const FetchAnime = async (query) => {
+		const temp = await fetch(`https://api.jikan.moe/v3/search/anime?q=${query}&order_by=title&sort=asc&limit=10`)
+			.then(res => res.json());
+
+		SetAnimeList(temp.results);
+	}
+
+	useEffect(() => {
+		GetTopAnime();
+	}, []);
+	
+	return (
+		<div className="App">
+			<Header />
+			<div className="content-wrap">
+				<Sidebar 
+					topAnime={topAnime} />
+				<MainContent
+					HandleSearch={HandleSearch}
+					search={search}
+					SetSearch={SetSearch}
+					animeList={animeList} />
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
