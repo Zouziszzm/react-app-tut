58d26b4cfc32dfec112f21ebd5f11d8509152de2
Al Farhaan khan I Inamdar <63239385+Zouziszzm@users.noreply.github.com>
2023-02-20 12:37:34 +0530

    a bit of moving things here and there
    still stuck at the side bar.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,39 @@
 import '../components/styles/sidebar.css'
 function Sidebar (){
   return (
-      <aside>
+      <aside className="side-Bar">
         <nav className="side-cont">
             <h3 className="side-head">Top Anime</h3>
-            <div className="side-a-list">
-                <a href="">Your Name</a>
-            </div>
+                <div className="card-wrap">
+
+                    <div className="card-item">
+                        <div className="card-inner">
+                            <div className="name">Your name</div>
+                            <div className="description">
+                                Lorem ipsum dolor, sit amet 
+                                consectetur adipisicing elit. Odio inventore, nisi 
+                                quis aspernatur at adipisci.
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="card-wrap">
+
+                    <div className="card-item">
+                        <div className="card-inner">
+                            <div className="name">Your name</div>
+                            <div className="description">
+                                Lorem ipsum dolor, sit amet 
+                                consectetur adipisicing elit. Odio inventore, nisi 
+                                quis aspernatur at adipisci.
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
         </nav>
       </aside>
   )
