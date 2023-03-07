d095cec74e5c338a0cdfa831f26c250ec4d39606
Al Farhaan khan I Inamdar <63239385+Zouziszzm@users.noreply.github.com>
2023-03-07 13:43:55 +0530

    reiniteated

---
+++ src/Components/main.jsx
@@ -0,0 +1,18 @@
+import '../../public/css/main.css'
+export default function Main() {
+  return (
+    <div className='main'>
+        <div className='heading-div'>
+            <h1 className='heading-style'>The Book Of Pleasure</h1>
+        </div>
+        <nav>
+            <div>
+                <ul>
+                    <li className='items'>Anime</li>
+                    <li className='items'>Movies</li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+  )
+}
